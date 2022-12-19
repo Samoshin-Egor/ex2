7174264485960944294b3698cfda196e721ebae6
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-19 13:41:15 +0300

    TestBall.java

---
+++ TestBall.java
@@ -0,0 +1,10 @@
+package ex2;
+import java.lang.*;
+public class Testball {
+    public static void main (String[] args) {
+        ball b1 = new ball (100,100);
+        System.out.println(b1);
+        b1.move(30,15);
+        System.out.println(b1);
+    }
+}
